9cbbf7c872fcfa1f46936c23545633499a4166b3
ymartin <ynmartin@gmail.com>
2016-09-05 11:23:50 +1000

    Inital

---
+++ test/calculator_test.js
@@ -0,0 +1,8 @@
+describe('Calculator', function() {
+    describe('add', function() {
+        describe('example functionality', function() {
+            it('should return something', function() {
+            });
+        });
+    });
+})
