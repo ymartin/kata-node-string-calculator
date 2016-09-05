86a28b440e56ab433db381d997f659b1a9c802b8
ymartin <ynmartin@gmail.com>
2016-09-05 11:41:44 +1000

    Added example assert

--- test/calculator_test.js
+++ test/calculator_test.js
@@ -2,6 +2,9 @@ describe('Calculator', function() {
     describe('add', function() {
         describe('example functionality', function() {
             it('should return something', function() {
+            	let result = 0;
+
+            	result.should.equal(0);
             });
         });
     });
